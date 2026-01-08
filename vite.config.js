41a7207e6aec97ab140bdfc53103be26704e2964
liu7say <liu7trace@gmail.com>
2026-01-08 17:45:02 +0800

    打包配置修改

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,34 @@ export default defineConfig({
 		},
 		cors: true,
 	},
+	build: {
+		chunkSizeWarningLimit: 1000,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						if (
+							id.includes('vue') ||
+							id.includes('pinia') ||
+							id.includes('vue-router') ||
+							id.includes('@vueuse')
+						) {
+							return 'vendor-vue';
+						}
+						if (id.includes('codemirror') || id.includes('@codemirror')) {
+							return 'vendor-codemirror';
+						}
+						if (
+							id.includes('jsondiffpatch') ||
+							id.includes('diff') ||
+							id.includes('lz-string')
+						) {
+							return 'vendor-utils';
+						}
+						return 'vendor';
+					}
+				},
+			},
+		},
+	},
 });
