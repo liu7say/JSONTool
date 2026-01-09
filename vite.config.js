5315d97e36d9fecc196962c40d265560e82f8926
刘旭 <liu7say@qq.com>
2026-01-09 14:20:36 +0800

    图标和logo优化

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './src/manifest.json';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue(), crx({ manifest })],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 	server: {
 		port: 5173,
 		strictPort: true,
